8b29df4de0233c89b2ac2b9137303f1ff6714ce1
Ian Albuquerque Raymundo da Silva <iars@br.ibm.com>
2018-09-18 17:24:19 -0300

    intro update

--- latex-clean.sh
+++ latex-clean.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 arg=${1:-.}
-exts="aux bcf dvi bbl blg brf idx ilg ind lof log lol lot out toc synctex.gz"
+exts="aux bcf run.xml dvi bbl blg brf idx ilg ind lof log lol lot out toc synctex.gz"
 
 if [ -d $arg ]; then
     for ext in $exts; do
